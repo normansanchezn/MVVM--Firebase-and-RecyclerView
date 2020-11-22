44e2fd44f31617618b91b4fe1e0b868a6c1e8df7
Norman SN <normansanchezn@gmail.com>
2020-11-22 14:03:52 -0600

    Configuration of project!
    Only I configure the project.

---
+++ MVVMRecyclerView/app/src/main/java/dev/norman/mvvm_firebase_recyclerview/MVVMApplication.kt
@@ -0,0 +1,10 @@
+package dev.norman.mvvm_firebase_recyclerview
+
+import android.app.Application
+
+class MVVMApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
